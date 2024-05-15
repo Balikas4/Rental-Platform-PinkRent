1ea4de50fea2e053db378ae6f0458ca7804c3939
Balikas <balikas4@gmail.com>
2024-05-15 14:47:03 +0300

    add: csrf trusted origins edit: base html

--- PinkRent/PinkRent/settings.py
+++ PinkRent/PinkRent/settings.py
@@ -17,6 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS=['https://pinkpink.lt', 'https://www.pinkpink.lt']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
