f2974d2c641a8b78095a33bbe883bdce45de8aa1
Balikas <balikas4@gmail.com>
2024-08-07 14:26:15 +0300

    edit: settings.py

--- PinkRent/PinkRent/settings.py
+++ PinkRent/PinkRent/settings.py
@@ -151,9 +151,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'PinkRent/static_root'
+STATIC_ROOT = BASE_DIR / 'static_root'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'PinkRent/media_root'
+MEDIA_ROOT = BASE_DIR / 'media_root'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
