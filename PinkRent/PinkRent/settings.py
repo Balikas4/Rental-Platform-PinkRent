f4532a6a6d5dcc4dbdfe2fe0b9660a203ba59986
Balikas <balikas4@gmail.com>
2024-10-16 14:45:58 +0300

    edit: debug

--- PinkRent/PinkRent/settings.py
+++ PinkRent/PinkRent/settings.py
@@ -26,7 +26,7 @@ CSRF_TRUSTED_ORIGINS=['https://upupam.world', 'https://www.upupam.world']
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'postgres', 'www.upupam.world', 'upupam.world']
 
